ce4d200f6f06e4f8e027096d03d8f945f79aeacd
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-05-31 12:04:04 +0800

    feat: get current location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,50 @@
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
+import { Text, Button, ActivityIndicator } from "react-native";
+import * as Location from "expo-location";
 
 export default function Home() {
+  const [location, setLocation] = useState<Location.LocationObject | undefined>(
+    undefined
+  );
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  //Get Location
+  const getLocation = async () => {
+    setIsLoading(true);
+
+    // Ask for permission to access location
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      setIsLoading(false);
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <Container>
-      <Text>Home</Text>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <Text>{text}</Text>
+      )}
     </Container>
   );
 }
