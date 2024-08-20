d97ae4c14f2ebfe129da9125990222b00f3032df
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-08-20 08:23:09 +0800

    chore: added needed libraries for development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 export default function App() {
-
-    return (
-        <>
-            <h1>Hello World!</h1>
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-xl">Hello World!</h1>
+    </>
+  );
 }
-
