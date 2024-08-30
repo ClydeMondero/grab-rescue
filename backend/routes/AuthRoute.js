a450f35ce646f4df502e5d1e7f6e2b9e7baac746
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-08-30 20:23:22 +0800

    feat: added user verification middleware

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { Login } = require("../controllers/AuthController");
+const { UserVerification } = require("../middlewares/AuthMiddleware");
 
+router.post("/", UserVerification);
 router.post("/login", Login);
 
 module.exports = router;
