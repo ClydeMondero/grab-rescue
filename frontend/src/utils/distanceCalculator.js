3e97c234238b64dbb97c310f0af8949a8c3e8312
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-09-25 07:01:41 +0800

    feat: utilities for getting distance between two location and generating user ids

---
+++ frontend/src/utils/distanceCalculator.js
@@ -0,0 +1,18 @@
+export const getDistance = (lat1, lon1, lat2, lon2) => {
+  const toRad = (x) => (x * Math.PI) / 180;
+  const R = 6371; //Earth's radius in km
+
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; //return distance in km
+};
