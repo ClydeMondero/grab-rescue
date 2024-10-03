dce11eea064519b53594a1298e2404c57c4f7baf
Bhenz Mharl L. Bartolome <bhenzmharlbartolome@gmail.com>
2024-10-03 22:56:29 +0800

    feat: add reset password form

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -4,6 +4,7 @@ const {
   VerifyEmail,
   RequestPasswordReset,
   ResetPassword,
+  ShowResetPasswordForm, // Import the new method
 } = require("../controllers/UserController");
 
 // Get users route
@@ -14,6 +15,9 @@ router.put("/verify/:token", VerifyEmail);
 
 // Request password reset route
 router.post("/forgot-password", RequestPasswordReset);
+
+// Show reset password form route
+router.get("/reset-password/:token", ShowResetPasswordForm); // New route for the form
 
 // Reset password route
 router.post("/reset-password/:token", ResetPassword);
