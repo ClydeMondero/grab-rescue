4a4cbfd827d2da18ba12c1e3ddb3b2a312469ccb
Bhenz Mharl L. Bartolome <bhenzmharlbartolome@gmail.com>
2024-10-04 11:17:56 +0800

    fix: transfer update user controller and route to UserController/Route

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,13 +1,20 @@
 const router = require("express").Router();
 const {
   GetUsers,
+  GetUser,
   VerifyEmail,
   RequestPasswordReset,
   ResetPassword,
+  UpdateUser,
+  UpdateUserEmail,
+  UpdateUserPassword,
 } = require("../controllers/UserController");
 
 // Get users route
 router.get("/get", GetUsers);
+
+// Get user route
+router.get("/get/:id", GetUser);
 
 // Verify email route
 router.put("/verify/:token", VerifyEmail);
@@ -17,5 +24,14 @@ router.post("/forgot-password", RequestPasswordReset);
 
 // Reset password route
 router.post("/reset-password/:token", ResetPassword);
+
+// Update user route
+router.put("/update/:id", UpdateUser);
+
+// Update user email route
+router.put("/updateEmail/:id", UpdateUserEmail);
+
+// Update user password route
+router.put("/updatePassword/:id", UpdateUserPassword);
 
 module.exports = router;
