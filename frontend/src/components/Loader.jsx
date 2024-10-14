faac07c398b134dcd2f86bd0805ed820bbb505bb
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-10-15 00:41:38 +0800

    feat: added loading indicator in map

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,12 @@
 import "ldrs/cardio";
 
-const Loader = ({ isLoading, color }) => {
+const Loader = ({ isLoading, color, size = 25 }) => {
   const loaderColor = color ? color : "#fff";
 
   return (
     <div aria-live="polite" aria-busy={isLoading}>
       {isLoading && (
-        <l-cardio size="25" color={loaderColor}>
+        <l-cardio size={size} color={loaderColor}>
           {" "}
         </l-cardio>
       )}
