227f6e5e9e4b35e8674f9ec53126752af63ba4ef
ClydeMondero <clydereinyelmondero0324@gmail.com>
2024-10-16 13:45:49 +0800

    feat: added ambulance model animation

--- frontend/src/components/RescuerMarker.jsx
+++ frontend/src/components/RescuerMarker.jsx
@@ -1,18 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import ambulanceModel from "../assets/ambulance/scene.gltf";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, useFrame } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
 
 const Model = ({ view }) => {
   const gltf = useLoader(GLTFLoader, ambulanceModel);
   const model = gltf.scene;
 
-  view == "top-down"
+  useFrame(() => {
+    model.position.x += 0.1;
+
+    if (model.position.x > 100) {
+      model.position.x = -100;
+    }
+  });
+
+  view === "top-down"
     ? model.rotation.set(Math.PI / 2, Math.PI, 0)
     : model.rotation.set(0, Math.PI / 2, 0);
 
-  view == "top-down"
+  view === "top-down"
     ? model.scale.set(0.5, 0.5, 0.5)
     : model.scale.set(1, 1, 1);
 
@@ -22,6 +30,20 @@ const Model = ({ view }) => {
     }
   });
 
+  // view == "top-down"
+  //   ? model.rotation.set(Math.PI / 2, Math.PI, 0)
+  //   : model.rotation.set(0, Math.PI / 2, 0);
+
+  // view == "top-down"
+  //   ? model.scale.set(0.5, 0.5, 0.5)
+  //   : model.scale.set(1, 1, 1);
+
+  // model.traverse((child) => {
+  //   if (child.isMesh) {
+  //     child.material.color.setHex(0x99ff99);
+  //   }
+  // });
+
   return <primitive object={model} />;
 };
 
@@ -29,7 +51,7 @@ const RescuerMarker = ({ view }) => {
   return (
     <Canvas
       style={{
-        width: "100px",
+        width: "75px",
         height: "75px",
         background: "transparent",
       }}
