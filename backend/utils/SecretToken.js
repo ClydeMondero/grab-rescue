7710a6e859c28aa453c87ca29f03b2ca97c3667d
markjames06 <153993161+markjames06@users.noreply.github.com>
2024-10-21 22:54:09 +0800

    feat: extract token, logs for adding rescuer, password modifications

--- backend/utils/SecretToken.js
+++ backend/utils/SecretToken.js
@@ -4,3 +4,8 @@ const jwt = require("jsonwebtoken");
 module.exports.createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: "1d" });
 };
+
+module.exports.getTokenSubject = async (token) => {
+  const payload = jwt.verify(token, process.env.TOKEN_KEY);
+  return payload.id;
+};
