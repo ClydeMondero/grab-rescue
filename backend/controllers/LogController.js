bfc873dd5fc707f93daae28ec5bd1eb839f41b5b
Bhenz Mharl L. Bartolome <bhenzmharlbartolome@gmail.com>
2024-10-21 17:56:37 +0800

    feat: log creation in user controller and auth controller

--- backend/controllers/LogController.js
+++ backend/controllers/LogController.js
@@ -1,18 +1,20 @@
 const pool = require("../config/db");
-const env = require("../config/env");
 
-module.exports.CreateLog = async (req, res) => {
-  const { userId, type, description } = req.body;
+module.exports.CreateLog = async ({ userId, action }) => {
   const q =
-    "INSERT INTO logs (`user_id`, `type`, `description`) VALUES (?, ?, ?)";
-  const values = [userId, type, description];
-  pool.query(q, values, (err, data) => {
-    if (err) {
-      console.error("Error inserting log:", err);
-      return res
-        .status(500)
-        .json({ error: "An error occurred while logging the action." });
-    }
-    return res.status(201).json({ success: true, logId: data.insertId });
-  });
+    "INSERT INTO logs (date_time, action, user_id) VALUES (NOW() AT TIME ZONE 'Asia/Manila', $1, $2) RETURNING id";
+  const values = [action, userId];
+
+  try {
+    const data = await pool.query(q, values);
+    return {
+      success: true,
+      logId: data.rows[0].id,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: "An error occurred while logging the action.",
+    };
+  }
 };
