f9360557eea4919c6097b1c3c9586951b1ad0ce9
rijin0408 <bhenzmharlbartolome@gmail.com>
2024-10-30 18:15:52 +0800

    feat: show no online rescuers online

--- frontend/src/components/Markers.jsx
+++ frontend/src/components/Markers.jsx
@@ -2,6 +2,7 @@ import { Marker, Popup } from "react-map-gl";
 import { FaLocationPin } from "react-icons/fa6";
 import { BiSolidAmbulance } from "react-icons/bi";
 import ambulance from "../assets/ambulance.png";
+import { useEffect, useState } from "react";
 
 const Markers = ({
   myMarker,
@@ -9,11 +10,24 @@ const Markers = ({
   nearestOtherMarker,
   markerType,
 }) => {
+  const [noOnlineRescuers, setNoOnlineRescuers] = useState(false);
+
+  useEffect(() => {
+    console.log("Updated otherMarkers:", otherMarkers.length);
+    console.log("Current markerType of myMarker:", markerType);
+    const onlineRescuers = otherMarkers.filter(
+      (marker) => marker.status === "online" && marker.role === "rescuer"
+    );
+    setNoOnlineRescuers(
+      markerType === "citizen" && onlineRescuers.length === 0
+    );
+  }, [otherMarkers, markerType]);
+
   return (
     <>
       {myMarker && (
         <Marker longitude={myMarker.longitude} latitude={myMarker.latitude}>
-          <div className="relative flex flex-col items-center justify-center ">
+          <div className="relative flex flex-col items-center justify-center">
             {markerType === "citizen" ? (
               <FaLocationPin className="text-3xl text-secondary red-pulse" />
             ) : (
@@ -54,6 +68,13 @@ const Markers = ({
               </Marker>
             )
         )}
+
+      {/* Display message if markerType is "citizen" and no rescuers are online */}
+      {noOnlineRescuers && (
+        <div className="absolute top-0 left-0 right-0 text-center text-lg text-red-600 font-semibold">
+          No Online Rescuers
+        </div>
+      )}
     </>
   );
 };
