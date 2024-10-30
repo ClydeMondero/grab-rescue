21ea79d94bef85481291f4b803396939184a9f4e
Bhenz Mharl L. Bartolome <bhenzmharlbartolome@gmail.com>
2024-10-30 23:13:11 +0800

    refactor: change privacypolicy file name to policy

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -7,7 +7,7 @@ import ResetPassword from "./ResetPasswordPage";
 import VerifyEmail from "./VerifyEmail";
 import NotFound from "./NotFound";
 import MultiStepForm from "./MultiStepForm";
-import PrivacyPolicy from "./PrivacyPolicy";
+import Policy from "./Policy";
 import TermsOfService from "./TermsOfService";
 export {
   MultiStepForm,
@@ -19,6 +19,6 @@ export {
   ForgotPassword,
   ResetPassword,
   VerifyEmail,
-  PrivacyPolicy,
+  Policy,
   TermsOfService,
 };
